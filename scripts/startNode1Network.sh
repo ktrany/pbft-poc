902976bafdec6913f219088660cf870b9af628da
Kenny Tran <trke1011@hs-karlsruhe.de>
2021-02-17 22:51:15 +0100

    workaround faulty vm

--- scripts/startNode1Network.sh
+++ scripts/startNode1Network.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # start node
-python3.9 node/index.py --id 1 --host 0.0.0.0 --port 10001 --path ./log/node1.log --pkLoc ./keys/pair1/private_key.pem --peerNodeAddrList 172.24.100.78:10000 172.24.100.162:10002 172.24.100.168:10003
+python3.9 node/index.py --id 0 --host 0.0.0.0 --port 10001 --path ./log/node1.log --pkLoc ./keys/pair1/private_key.pem --peerNodeAddrList 172.24.100.78:10000 172.24.100.162:10002 172.24.100.168:10003
