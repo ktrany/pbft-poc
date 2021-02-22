f0c58eedd8f7893836ae528cefe52eeed826878d
Kenny Tran <trke1011@hs-karlsruhe.de>
2021-02-22 21:06:36 +0100

    script for monitoring created

--- scripts/getResourceUtilization.sh
+++ scripts/getResourceUtilization.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-CPU_USAGE=$(LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
+CPU_USAGE=$(mpstat 1 1 | awk '/all/{ print 100 - $NF; exit; }')
 MEMORY_USAGE=$(free -m | awk 'NR==2 {print $3}')
+
+MEMORY_USAGE_PERCENT=`bc <<< "scale=2; $MEMORY_USAGE/1993"`
 
 DATE=$(date "+%Y-%m-%d %H:%M")
 
-RES_USAGE="$DATE $CPU_USAGE $(($MEMORY_USAGE / 1993))"
+RES_USAGE="$DATE $CPU_USAGE $MEMORY_USAGE_PERCENT"
 
 echo $RES_USAGE >> ./output/cpu_usage.out
