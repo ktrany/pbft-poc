6d24aee849a3b88a0820317517edad516b4904b3
Kenny Tran <trke1011@hs-karlsruhe.de>
2021-02-22 20:45:40 +0100

    script for monitoring created

--- scripts/getResourceUtilization.sh
+++ scripts/getResourceUtilization.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-CPU_USAGE=$(echo | awk -v c="${cores}" -v l="${load}" '{print l*100/c}' | awk -F. '{print $1}')
+CPU_USAGE=$(LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')
 MEMORY_USAGE=$(free -m | awk 'NR==2 {print $3}')
 
 DATE=$(date "+%Y-%m-%d %H:%M")
 
-RES_USAGE="$DATE $CPU_USAGE (($MEMORY_USAGE / 1993))"
+RES_USAGE="$DATE $CPU_USAGE $(($MEMORY_USAGE / 1993))"
 
 echo $RES_USAGE >> ./output/cpu_usage.out
